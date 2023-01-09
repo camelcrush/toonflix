0a5b892629d9657f7b83356c6422fd8664743e65
camelcrush <dkxm0927@naver.com>
2023-01-09 21:34:05 +0900

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
-  // build로 자동 완성
+  const App({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    // MaterialApp or CupertinoApp 중 선택, MaterialApp 추천
-    return MaterialApp(
-      // scaffold : 레이아웃 위젯
-      home: Scaffold(
-          backgroundColor: Color(0xFF181818),
-          body: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 40,
-            ),
-            child: Column(children: [
-              SizedBox(
-                height: 80,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Hey Selena',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                      Text(
-                        'Welcome back',
-                        style: TextStyle(
-                          color: Color.fromRGBO(255, 255, 255, 0.5),
-                          fontSize: 18,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              )
-            ]),
-          )),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
